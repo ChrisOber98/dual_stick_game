193ea27c6302507c28aa758f8bfe821faf96db81
ChrisOber98 <Christopher.dane.ober@gmail.com>
2026-09-20 17:56:33 -0400

    Add settings.py file and add window settings

--- game.py
+++ game.py
@@ -1,24 +1,42 @@
 import pygame
 
+from settings import Settings
+
 class Game:
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Dual Stick Game")
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.window_width, self.settings.window_height))
+        pygame.display.set_caption(self.settings.winodw_caption)
 
         self.running = True
 
     def run_game(self):
         while self.running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-            self.screen.fill((0, 0, 0))
-            pygame.display.flip()
+            self._check_events()
+            self._update_game()
+            self._draw_screen()
 
         pygame.quit()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._check_quit_events()
+
+    def _check_quit_events(self):
+        self.running = False
+
+    def _update_game(self):
+        pass
+
+    def _draw_screen(self):
+        self.screen.fill(self.settings.window_bg_color)
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     my_game = Game()
---
+++ settings.py
@@ -0,0 +1,6 @@
+class Settings:
+    def __init__(self):
+        self.window_height = 800
+        self.window_width = 1200
+        self.window_bg_color = (255, 255, 255)
+        self.winodw_caption = "Dual Stick Game"
